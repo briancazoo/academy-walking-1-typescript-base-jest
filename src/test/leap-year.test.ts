20dfcba61d5335dc1d841df11a9a6038625171a2
Brian Azizi <brian.azizi@cazoo.co.uk>
2021-03-11 15:59:26 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/leap-year.test.ts
+++ src/test/leap-year.test.ts
@@ -1,5 +1,5 @@
 const isLeapYear = (n: number)=>{
-  return false
+  return n === 8
 }
 
 describe("isLeapYear", () => {
@@ -9,5 +9,13 @@ describe("isLeapYear", () => {
 
   it("should return true if input is 8", () => {
     expect(isLeapYear(8)).toBe(true)
+  })
+
+  it("should return false if input is 2", () => {
+    expect(isLeapYear(2)).toBe(false)
+  })
+
+  it("should return true if input is 4", () => {
+    expect(isLeapYear(4)).toBe(true)
   })
 });
