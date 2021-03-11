3f4dd866a44ce7260ff48650ec4741658d0cc30d
Brian Azizi <brian.azizi@cazoo.co.uk>
2021-03-11 15:26:20 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/fizzbuzz.test.ts
+++ src/test/fizzbuzz.test.ts
@@ -1,20 +1,25 @@
-import {Example} from "../main/example";
-const fizzbuzz = (n: number)=> {
-    return "1"
-}
+import { Example } from "../main/example";
+
+const fizzbuzz = (n: number) => {
+  return n.toString();
+};
 
 describe("fizzbuzz", () => {
-    it("should return '1' when we pass 1", () => {
-        const output = fizzbuzz(1)
-        const expected = "1"
-        expect(output).toBe(expected)
-    })
+  it("should return '1' when we pass 1", () => {
+    const output = fizzbuzz(1);
+    const expected = "1";
+    expect(output).toBe(expected);
+  });
 
-    it("should return '2' when we pass 2", () => {
-        const output = fizzbuzz(2)
-        const expected = "2"
-        expect(output).toBe(expected)
-    })
+  it("should return '2' when we pass 2", () => {
+    const output = fizzbuzz(2);
+    const expected = "2";
+    expect(output).toBe(expected);
+  });
 
-})
-
+  it("should return 'fizz' when we pass 3", () => {
+    const output = fizzbuzz(3);
+    const expected = "fizz";
+    expect(output).toBe(expected);
+  });
+});
