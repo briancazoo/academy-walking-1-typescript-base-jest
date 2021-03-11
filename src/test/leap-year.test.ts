34e23833438b3ec56077035fba9a1678b3825fb0
Zahra Hassan <zahra.hassan@zahra.hassan.macbook>
2021-03-11 15:57:39 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/leap-year.test.ts
+++ src/test/leap-year.test.ts
@@ -1,6 +1,13 @@
+const isLeapYear = (n: number)=>{
+  return false
+}
 
 describe("isLeapYear", () => {
   it("should return false if input is 1", () => {
     expect(isLeapYear(1)).toBe(false)
+  })
+
+  it("should return true if input is 8", () => {
+    expect(isLeapYear(8)).toBe(true)
   })
 });
