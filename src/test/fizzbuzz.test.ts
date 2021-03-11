a3d2dbdb3b175da738e8abdbf97627244da8cae2
Zahra Hassan <zahra.hassan@zahra.hassan.macbook>
2021-03-11 15:21:23 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/fizzbuzz.test.ts
+++ src/test/fizzbuzz.test.ts
@@ -1,4 +1,7 @@
 import {Example} from "../main/example";
+const fizzbuzz = (n: number)=> {
+    return "1"
+}
 
 describe("fizzbuzz", () => {
     it("should return '1' when we pass 1", () => {
@@ -6,4 +9,12 @@ describe("fizzbuzz", () => {
         const expected = "1"
         expect(output).toBe(expected)
     })
+
+    it("should return '2' when we pass 2", () => {
+        const output = fizzbuzz(2)
+        const expected = "2"
+        expect(output).toBe(expected)
+    })
+
 })
+
