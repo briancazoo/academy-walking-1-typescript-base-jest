4086d1d492d4b9e5dedec856c1632270b13f2b4d
Brian Azizi <brian.azizi@cazoo.co.uk>
2021-03-18 16:06:37 +0000

    Formatting

--- src/test/calculator.test.ts
+++ src/test/calculator.test.ts
@@ -1,47 +1,50 @@
 function stringCalculator(numbers: string) {
-    if(numbers === '//;') return 0;
-    if(numbers === '//;\n0;1') return 1;
-    if(numbers === '//;\n0;2') return 2;
-    const delimiter = ',';
-    return numbers.replace('\n', delimiter).split(delimiter).reduce((total, number) => Number(number) + total, 0)
+  if (numbers === "//;") return 0;
+  if (numbers === "//;\n0;1") return 1;
+  if (numbers === "//;\n0;2") return 2;
+  const delimiter = ",";
+  return numbers
+    .replace("\n", delimiter)
+    .split(delimiter)
+    .reduce((total, number) => Number(number) + total, 0);
 }
 
 describe("stringCalculator", () => {
-    it.each([
-        ["", 0],
-        ["1", 1],
-        ["2",2],
-        ["3", 3],
-    ])('takes %s and returns %d', (input, expected) => {
-        expect(stringCalculator(input)).toBe(expected)
-    })
+  it.each([
+    ["", 0],
+    ["1", 1],
+    ["2", 2],
+    ["3", 3],
+  ])("takes %s and returns %d", (input, expected) => {
+    expect(stringCalculator(input)).toBe(expected);
+  });
 
-    it.each([
-        ["0,1", 1],
-        ["0,2", 2],
-        ["0,3",3],
-        ["1,1", 2],
-        ["1,2", 3],
-        ["1,3", 4],
-        ["2,1", 3],
-        ["10,10", 20]
-    ])("takes %s and returns %d", (input, expected) => {
-        expect(stringCalculator(input)).toBe(expected)
-    })
+  it.each([
+    ["0,1", 1],
+    ["0,2", 2],
+    ["0,3", 3],
+    ["1,1", 2],
+    ["1,2", 3],
+    ["1,3", 4],
+    ["2,1", 3],
+    ["10,10", 20],
+  ])("takes %s and returns %d", (input, expected) => {
+    expect(stringCalculator(input)).toBe(expected);
+  });
 
-    it.each([
-        ["0\n1", 1],
-        ["0\n2", 2],
-        ["0\n3", 3],
-    ])("takes %s and returns %d", (input, expected) =>{
-        expect(stringCalculator(input)).toBe(expected)
-    })
+  it.each([
+    ["0\n1", 1],
+    ["0\n2", 2],
+    ["0\n3", 3],
+  ])("takes %s and returns %d", (input, expected) => {
+    expect(stringCalculator(input)).toBe(expected);
+  });
 
-    it.each([
-        ["//;", 0],
-        ["//;\n0;1", 1],
-        ["//;\n0;2", 2],
-    ])("takes %s and returns %d", (input, expected) =>{
-        expect(stringCalculator(input)).toBe(expected)
-    })
-})
+  it.each([
+    ["//;", 0],
+    ["//;\n0;1", 1],
+    ["//;\n0;2", 2],
+  ])("takes %s and returns %d", (input, expected) => {
+    expect(stringCalculator(input)).toBe(expected);
+  });
+});
