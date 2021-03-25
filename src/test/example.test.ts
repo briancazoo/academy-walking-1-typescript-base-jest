7ad260f961cce18b574188948f90201ad1306c00
Paul <paul.millen@cazoo.co.uk>
2021-03-25 15:05:44 +0000

    mob next [ci-skip]

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,8 +1,10 @@
-import {Example} from "../main/example";
+const numerals = (number: number): string => {
+    return 'I'
+}
 
 describe('example test', () => {
     it("should give sum of a + b when adding a and b", () => {
-        let example: Example = new Example();
-        expect(example.add(1, 2)).toBe(3);
+        const result = numerals(1)
+        expect(result).toBe('I');
     })
 })
