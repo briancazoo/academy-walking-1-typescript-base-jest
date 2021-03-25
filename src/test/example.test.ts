3f3060c1437164ce6ba03a1c1e3202aa08d4634a
Brian Azizi <brian.azizi@cazoo.co.uk>
2021-03-25 16:03:13 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,14 +1,29 @@
 const numerals = (number: number): string => {
+  const bases: { [key: number]: string } = {
+    0: "",
+    5: "V",
+    10: "X",
+  };
+  let key = 0;
+  if (0 <= number && number < 5) {
+    key = 0;
+  } else {
+    key = 5;
+  }
+
+  const base = bases[key];
+  const nextBase = bases[key + 5];
+
   let result = ["", "I", "II", "III"];
-  if (number === 10) {
-      return "X"
-  }
+  // if (number === 10) {
+  //   return "X";
+  // }
 
-  if (number >= 5) {
-      return "V" + result[number - 5] 
-  }
 
-  return result[number];
+  if (number % 5 === 4) {
+    return "I" + nextBase;
+  }
+  return base + result[number % 5];
 };
 
 describe("example test", () => {
@@ -21,6 +36,7 @@ describe("example test", () => {
     [6, "VI"],
     [7, "VII"],
     [8, "VIII"],
+    [9, "IX"],
     [10, "X"],
   ] as const;
 
