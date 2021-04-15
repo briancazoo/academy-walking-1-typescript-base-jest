9ebb1000f87bbd0f3c0b48c9fc74940003ce17bd
Brian Azizi <brian.azizi@cazoo.co.uk>
2021-04-15 14:55:54 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/tictactoe-v2.test.ts
+++ src/test/tictactoe-v2.test.ts
@@ -1,3 +1,22 @@
-describe("TicTacToe", () => {
+enum Mark {
+  O = "O",
+  X = "X",
+}
 
-})
+enum Position {}
+
+interface Board {
+  positions: Map<Position, Mark>;
+  placeMark: (mark: Mark, position: Position) => void;
+}
+
+interface Game {
+  board: Board;
+}
+
+describe("TicTacToe", () => {
+  test("X plays first", () => {
+    const game = new Game();
+    game.play(Position.TopLeft);
+  });
+});
